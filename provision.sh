6284799fc6741dbe0ac66a78efe5782951434557
David Gwartney <david.gwartneuy@gmail.com>
2020-08-26 22:13:24 -0700

    Revise to work with Ubuntu with the latest updates

--- provision.sh
+++ provision.sh
@@ -10,9 +10,9 @@ sudo apt install -y python3-dev python3-pip
 #
 # Update to Python 3.6
 #
-# sudo add-apt-repository -y ppa:jonathonf/python-3.6
-# sudo apt-get update
-# sudo apt-get install -y python3.6
+sudo add-apt-repository -y ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install -y python3.6
 
 #
 # Bootstrap installation of PIP
@@ -23,16 +23,16 @@ sudo python3 /tmp/setup.py
 #
 # Downgrade PIP to earlier version that works
 #
-sudo pip install --upgrade --force-reinstall pip==9.0.3
+#sudo pip install --upgrade --force-reinstall pip==9.0.3
 
 #
 # Upgrade packages required by RASA
 #
-sudo pip install --upgrade PyJWT
+#sudo pip install --upgrade PyJWT
 
 #
 # Install the RASA packages
 #
-pip install rasa-x --extra-index-url https://pypi.rasa.com/simple
+pip install rasa --extra-index-url https://pypi.rasa.com/simple
 
 echo "Done!"
